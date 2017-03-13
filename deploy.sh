9a417b29ef09ba79a935a486caaf258121464e17
Oleg Ignatov <oleg.b.ignatov@gmail.com>
2017-03-14 01:22:48 +0300

    changed deploy.sh so docker build command is not run

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #docker build -t asverasver/sample-node .
-docker push asverasver/sample-node
+#docker push asverasver/sample-node
 
 ssh deploy@104.198.161.225
 docker pull asverasver/sample-node:latest
@@ -8,4 +8,4 @@ docker stop web || true
 docker rm web || true
 docker rmi asverasver/sample-node:current || true
 docker tag asverasver/sample-node:latest asverasver/sample-node:current
-docker run -d --net app --restart always --name web -p 80:80 asverasver/sample-node:current
+docker run -d --net lab6net --restart always --name web -p 80:80 asverasver/sample-node:current
